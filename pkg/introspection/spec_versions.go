29d6ca4bda6c231d9196cc7d3f93fa0595b6518b
Ben Weintraub <benweint@gmail.com>
2024-05-22 21:02:03 -0700

    adding forgotten file

---
+++ pkg/introspection/spec_versions.go
@@ -0,0 +1,42 @@
+package introspection
+
+import (
+	"fmt"
+	"strings"
+)
+
+type SpecVersion struct {
+	name                 string
+	HasSpecifiedByURL    bool
+	HasIsRepeatable      bool
+	HasSchemaDescription bool
+}
+
+var specVersions = map[string]SpecVersion{
+	"june2018": {
+		name: "june2018",
+	},
+	"october2021": {
+		name:                 "october2021",
+		HasSpecifiedByURL:    true,
+		HasIsRepeatable:      true,
+		HasSchemaDescription: true,
+	},
+}
+
+func ParseSpecVersion(raw string) (SpecVersion, error) {
+	sv, ok := specVersions[raw]
+	if !ok {
+		return SpecVersion{}, fmt.Errorf("invalid spec version '%s', known versions are %s", raw, strings.Join(knownVersions(), ", "))
+	}
+
+	return sv, nil
+}
+
+func knownVersions() []string {
+	var result []string
+	for name := range specVersions {
+		result = append(result, name)
+	}
+	return result
+}
