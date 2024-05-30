ef0c16e26888fc0b926218dbbda5438779b3cf52
Ben Weintraub <benweint@gmail.com>
2024-05-30 16:05:13 -0700

    add a --named flag to ls fields to list fields with a specific name

--- NOTES.md
+++ NOTES.md
@@ -6,7 +6,7 @@
 ## Documentation
 
 - [x] Make `gquil help <subcommand>` show help instead of an error
-- [ ] Make help text link back to a path for feedback, issues
+- [x] Make help text link back to a path for feedback, issues
 - [ ] Add examples to the in-tool documentation
 - [ ] Add a manpage
 
@@ -21,10 +21,11 @@
 
 ## Error handling
 
-- [ ] Ensure that schema parse / validation errors actually point back to the source location
+- [x] Ensure that schema parse / validation errors actually point back to the source location
 
 ## Features
 
-- [ ] Add a --named arg to ls fields command
+- [x] Add a --named arg to ls fields command
 - [ ] Make --interfaces-as-unions work everywhere that --from does
 - [ ] Add a --depth-reverse flag to graph filtering options
+- [ ] Add a --with-directive flag to filter types, fields by directives
--- pkg/commands/ls_fields.go
+++ pkg/commands/ls_fields.go
@@ -11,6 +11,7 @@ type LsFieldsCmd struct {
 	OnType        string `name:"on-type" group:"filtering" help:"Only include fields which appear on the specified type."`
 	OfType        string `name:"of-type" group:"filtering" help:"Only include fields of the specified type. List and non-null types will be treated as being of their underlying wrapped type for the purposes of this filtering."`
 	ReturningType string `name:"returning-type" group:"filtering" help:"Only include fields which may return the specified type. Interface or union-typed fields may possibly return their implementing or member types. List and non-null fields are unwrapped for the purposes of this filtering."`
+	Named         string `name:"named" group:"filtering" help:"Only include fields with the given name (matched against the field name only, not including type name)."`
 	IncludeArgs   bool   `name:"include-args" group:"output" help:"Include argument definitions in human-readable output. Has no effect with --json."`
 	IncludeDirectivesOption
 	OutputOptions
@@ -42,6 +43,9 @@ func (c LsFieldsCmd) Run(ctx Context) error {
 				continue
 			}
 			if c.ReturningType != "" && !fieldMightReturn(s, f, c.ReturningType) {
+				continue
+			}
+			if c.Named != "" && c.Named != f.Name {
 				continue
 			}
 			f.Name = t.Name + "." + f.Name
